751892ffffe4452ce1ed706555ec28e6113b4bfa
kzhukov91 <k.zhukov91@yandex.ru>
2024-02-28 13:37:10 +0300

    rename files

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,13 +1,10 @@
-"""Игра угадай число
-Компьютер сам загадывает и сам угадывает число
-"""
-
 import numpy as np
 
-
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
-
+def game_core_v2(number: int = 1) -> int:
+    """Сначала устанавливаем любое random число, а потом уменьшаем
+    или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток
+       
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -15,17 +12,20 @@ def random_predict(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-
-    while True:
+    predict = np.random.randint(1, 101)
+    
+    while number != predict:
         count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+
     return count
 
 
-def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+def score_game(game_core_v2) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -38,16 +38,12 @@ def score_game(random_predict) -> int:
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(game_core_v2(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
-    return score
-
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+    
 #Run benchmarking to score effectiveness of all algorithms
-print('Run benchmarking for random_predict: ', end='')
-score_game(random_predict)
-
-if __name__ == "__main__":
-    # RUN
-    score_game(random_predict)
+print('Run benchmarking for game_core_v2: ', end='')
+score_game(game_core_v2)
